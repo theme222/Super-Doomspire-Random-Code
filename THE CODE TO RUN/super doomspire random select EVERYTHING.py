d80aa9301b8d245ebe896e901dc07254bba4135e
theme222 <65989329+theme222@users.noreply.github.com>
2021-01-17 11:29:15 +0700

    Add files via upload

---
+++ THE CODE TO RUN/super doomspire random select EVERYTHING.py
@@ -0,0 +1,24 @@
+from random import choice
+
+weaponslotlist = []
+rocketlist = ['Rocket Launcher', 'Scope Shot', 'Starblaster', 'Shadow Dragon', 'Hoss Hog', 'Arm Cannon', 'Rock Star',
+              'Laser Cannon', 'Crystal Blaster']
+swordlist = ["normal", "fire", "bigboi", "homerunner", 'tennis', 'brick', "vox", "pan", "ice", "shadow", "heart"]
+bomblist = ['Bomb', 'Square Bomb', 'Remote Detonator', 'Shadow Bomb', 'Sticky Bomb']
+balllist = ['Superball', 'Coconut', 'Boomerang', 'Hat-A-Rang', 'Lava Ball', 'Shadow Shuriken', 'Snowball',
+            'Paintball Gun', 'Slingshot', 'Bow']
+trowellist = ['Trowel', 'Truss Trowel', 'Spike Trowel', 'Bridge Trowel', 'Ball Turret', 'Cage Trowel', 'Shadow Clone',
+              'Trampoline Trowel']
+extraeventlist = ['bombonly', 'rocketonly', 'nosword', 'notrowel+ball', 'nowalking', "don'ttakedamage"]
+lolhaha = ["weapon", 'bomb', 'sword', 'ball', 'trowel']
+for i in range(1, 6):
+    a = choice(lolhaha)
+    lolhaha.remove(a)
+    weaponslotlist.append(a)
+print(weaponslotlist)
+print("sword = ", choice(swordlist))
+print("bomb = ", choice(bomblist))
+print("rocket = ", choice(rocketlist))
+print("ball = ", choice(balllist))
+print("trowel = ", choice(trowellist))
+print("challenge = ",choice(extraeventlist))
